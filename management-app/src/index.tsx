b68fc1a25d7ca24514253428e9bf25226aeeca26
Javier García <99670835+jagarmon@users.noreply.github.com>
2024-01-21 23:47:02 +0100

    navbar and routing

--- management-app/src/index.tsx
+++ management-app/src/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app/App';
+import { BrowserRouter } from 'react-router-dom';
 
 const domNode = document.getElementById('root');
 const root = ReactDOM.createRoot(domNode!);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
